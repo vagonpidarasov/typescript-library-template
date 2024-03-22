e7faa61de28296329dc11a967536746da4b4b5f9
Stanislav Beresnev <sberesnev@newrelic.com>
2024-03-22 11:40:08 +0100

    feat: Test onClick

--- src/Button.spec.tsx
+++ src/Button.spec.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Button } from './Button';
 
 describe('Button', () => {
+  const onClick = jest.fn();
+
+  afterEach(() => {
+    onClick.mockClear();
+  });
+
   it('should be defined', () => {
     expect(Button).toBeTruthy();
   });
@@ -10,5 +16,11 @@ describe('Button', () => {
   it('should render', () => {
     render(<Button>Click me</Button>);
     expect(screen.getByText('Click me')).toBeInTheDocument();
+  });
+
+  it('should handle click', () => {
+    render(<Button onClick={onClick}>Click me</Button>);
+    fireEvent.click(screen.getByText('Click me'));
+    expect(onClick).toBeCalled();
   });
 });
